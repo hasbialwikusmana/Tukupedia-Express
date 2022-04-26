9f060efb0c2d2773718f0a084e20ea3012f76ac4
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-27 02:02:48 +0700

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,6 +6,6 @@ router.get("/:id", productsController.getProductsById);
 router.post("/", productsController.postProducts);
 router.put("/:id", productsController.putProducts);
 router.delete("/:id", productsController.deleteProducts);
-router.get("/:name", productsController.getProductsByName);
+router.get("/name/:name", productsController.getProductsByName);
 
 module.exports = router;
