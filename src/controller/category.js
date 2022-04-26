86f45333565bc7bcd1a2d27310dcab2ffaff9e33
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-27 02:02:33 +0700

    Update category.js

--- src/controller/category.js
+++ src/controller/category.js
@@ -1,53 +1,67 @@
 const categoryModels = require("../models/category");
 const helper = require("../helper/response");
 
-exports.getCategory = async (req, res) => {
+exports.getCategory = async (req, res, next) => {
   try {
     const result = await categoryModels.getCategory();
-    return helper.response(res, 200, "Success Get All Category", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    helper.response(res, 200, "Success Get Data Category", result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-
-exports.getCategoryById = async (req, res) => {
+exports.getCategoryById = async (req, res, next) => {
   try {
-    const result = await categoryModels.getCategoryById(req.params.id);
-    return helper.response(res, 200, "Success Get Category By Id", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const id = req.params.id;
+    const result = await categoryModels.getCategoryById(id);
+    helper.response(res, 200, `Success Get Data Category By Id ${id}`, result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-
-exports.postCategory = async (req, res) => {
+exports.postCategory = async (req, res, next) => {
   try {
-    const result = await categoryModels.postCategory(req.body);
-    return helper.response(res, 200, "Success Post Category", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const setData = {
+      id: req.body.id,
+      name_category: req.body.name_category,
+    };
+    const result = await categoryModels.postCategory(setData);
+    helper.response(res, 200, "Success Post Data Category", result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-exports.putCategory = async (req, res) => {
+exports.putCategory = async (req, res, next) => {
   try {
-    const result = await categoryModels.putCategory(req.params.id, req.body);
-    return helper.response(res, 200, "Success Edit Category", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const id = req.params.id;
+    const setData = {
+      name_category: req.body.name_category,
+    };
+    const result = await categoryModels.putCategory(id, setData);
+    helper.response(res, 200, `Success Update Data Category ${id}`, result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-exports.deleteCategory = async (req, res) => {
+exports.deleteCategory = async (req, res, next) => {
   try {
-    const result = await categoryModels.deleteCategory(req.params.id);
-    return helper.response(res, 200, "Success Delete Category", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const id = req.params.id;
+    const result = await categoryModels.deleteCategory(id);
+    helper.response(res, 200, `Success Delete Data Category ${id}`, result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-exports.getCategoryByName = async (req, res) => {
+exports.getCategoryByName = async (req, res, next) => {
   try {
-    const result = await categoryModels.getCategoryByName(req.params.name);
-    return helper.response(res, 200, "Success Get Category By Name", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const name = req.params.name;
+    const result = await categoryModels.getCategoryByName(name);
+    helper.response(
+      res,
+      200,
+      `Success Get Data Category By Name ${name}`,
+      result
+    );
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
