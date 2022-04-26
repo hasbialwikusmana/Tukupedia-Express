0acc67169a6f28ba243cd42c77cd8b8df7393f50
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-27 02:02:36 +0700

    Update products.js

--- src/controller/products.js
+++ src/controller/products.js
@@ -1,56 +1,79 @@
 const productsModels = require("../models/products");
 const helper = require("../helper/response");
 
-exports.getProducts = async (req, res) => {
+exports.getProducts = async (req, res, next) => {
   try {
     const result = await productsModels.getProducts();
-    return helper.response(res, 200, "Success Get All Products", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    helper.response(res, 200, "Success Get Data Products", result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-
-exports.getProductsById = async (req, res) => {
+exports.getProductsById = async (req, res, next) => {
   try {
-    const result = await productsModels.getProductsById(req.params.id);
-    return helper.response(res, 200, "Success Get Products By Id", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const id = req.params.id;
+    const result = await productsModels.getProductsById(id);
+    helper.response(res, 200, `Success Get Data Products By Id ${id}`, result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-
-exports.getProductsByName = async (req, res) => {
+exports.postProducts = async (req, res, next) => {
   try {
-    const result = await productsModels.getProductsByName(req.params.name);
-    return helper.response(res, 200, "Success Get Products By Name", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const setData = {
+      id: req.body.id,
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      stock: req.body.stock,
+      created_at: req.body.created_at,
+      // image: req.body.image,
+      // category_id: req.body.category_id,
+    };
+    const result = await productsModels.postProducts(setData);
+    helper.response(res, 200, "Success Post Data Products", result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-
-exports.postProducts = async (req, res) => {
+exports.putProducts = async (req, res, next) => {
   try {
-    const result = await productsModels.postProducts(req.body);
-    return helper.response(res, 200, "Success Post Products", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const id = req.params.id;
+    const setData = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      stock: req.body.stock,
+      created_at: req.body.created_at,
+      // image: req.body.image,
+      // category_id: req.body.category_id,
+    };
+    const result = await productsModels.putProducts(id, setData);
+    helper.response(res, 200, `Success Update Data Products ${id}`, result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-
-exports.putProducts = async (req, res) => {
+exports.deleteProducts = async (req, res, next) => {
   try {
-    const result = await productsModels.putProducts(req.params.id, req.body);
-    return helper.response(res, 200, "Success Edit Products", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const id = req.params.id;
+    const result = await productsModels.deleteProducts(id);
+    helper.response(res, 200, `Success Delete Data Products ${id}`, result);
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
-
-exports.deleteProducts = async (req, res) => {
+exports.getProductsByName = async (req, res, next) => {
   try {
-    const result = await productsModels.deleteProducts(req.params.id);
-    return helper.response(res, 200, "Success Delete Products", result);
-  } catch (err) {
-    return helper.response(res, 400, "Bad Request", err);
+    const name = req.params.name;
+    const result = await productsModels.getProductsByName(name);
+    helper.response(
+      res,
+      200,
+      `Success Get Data Products By Name ${name}`,
+      result
+    );
+  } catch (error) {
+    helper.response(res, 400, "Bad Request", error);
   }
 };
