b5a32080d45b94292542c9e6330ee4bc0cae27b0
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-26 22:46:28 +0700

    Create products.js

---
+++ src/routes/products.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const productsController = require("../controller/products");
+
+router.get("/", productsController.getProducts);
+router.get("/:id", productsController.getProductsById);
+router.post("/", productsController.postProducts);
+router.put("/:id", productsController.putProducts);
+router.delete("/:id", productsController.deleteProducts);
+router.get("/:name", productsController.getProductsByName);
+
+module.exports = router;
