067574a33c22604910d540ad47eccb78c3193bb2
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-26 22:46:22 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-// const products = require("./routes/products");
+const products = require("./routes/products");
 const category = require("./routes/category");
 
-// router.use("/products", productRouter);
+router.use("/products", products);
 router.use("/category", category);
 
 module.exports = router;
