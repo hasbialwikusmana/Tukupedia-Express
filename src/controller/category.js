590f8c7888261c57f1a0a6064f0a97795191c210
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-29 10:20:25 +0700

    Update category.js

--- src/controller/category.js
+++ src/controller/category.js
@@ -1,12 +1,33 @@
+const createError = require("http-errors");
 const categoryModels = require("../models/category");
+const errorServ = new createError.InternalServerError();
 const helper = require("../helper/response");
 
 exports.getCategory = async (req, res, next) => {
   try {
-    const result = await categoryModels.getCategory();
-    helper.response(res, 200, "Success Get Data Category", result);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const offset = (page - 1) * limit;
+    const result = await categoryModels.getCategory({ offset, limit });
+    // pagination
+    const {
+      rows: [count],
+    } = await categoryModels.countCategory();
+    const totalData = parseInt(count.total);
+    const totalPage = Math.ceil(totalData / limit);
+
+    res.status(200).json({
+      pagination: {
+        currentPage: page,
+        limit,
+        totalData,
+        totalPage,
+      },
+      data: result.rows[0],
+    });
   } catch (error) {
-    helper.response(res, 400, "Bad Request", error);
+    console.log(error);
+    next(errorServ);
   }
 };
 exports.getCategoryById = async (req, res, next) => {
@@ -15,7 +36,8 @@ exports.getCategoryById = async (req, res, next) => {
     const result = await categoryModels.getCategoryById(id);
     helper.response(res, 200, `Success Get Data Category By Id ${id}`, result);
   } catch (error) {
-    helper.response(res, 400, "Bad Request", error);
+    console.log(error);
+    next(errorServ);
   }
 };
 exports.postCategory = async (req, res, next) => {
@@ -27,7 +49,8 @@ exports.postCategory = async (req, res, next) => {
     const result = await categoryModels.postCategory(setData);
     helper.response(res, 200, "Success Post Data Category", result);
   } catch (error) {
-    helper.response(res, 400, "Bad Request", error);
+    console.log(error);
+    next(errorServ);
   }
 };
 exports.putCategory = async (req, res, next) => {
@@ -39,7 +62,8 @@ exports.putCategory = async (req, res, next) => {
     const result = await categoryModels.putCategory(id, setData);
     helper.response(res, 200, `Success Update Data Category ${id}`, result);
   } catch (error) {
-    helper.response(res, 400, "Bad Request", error);
+    console.log(error);
+    next(errorServ);
   }
 };
 exports.deleteCategory = async (req, res, next) => {
@@ -48,7 +72,8 @@ exports.deleteCategory = async (req, res, next) => {
     const result = await categoryModels.deleteCategory(id);
     helper.response(res, 200, `Success Delete Data Category ${id}`, result);
   } catch (error) {
-    helper.response(res, 400, "Bad Request", error);
+    console.log(error);
+    next(errorServ);
   }
 };
 exports.getCategoryByName = async (req, res, next) => {
@@ -62,6 +87,7 @@ exports.getCategoryByName = async (req, res, next) => {
       result
     );
   } catch (error) {
-    helper.response(res, 400, "Bad Request", error);
+    console.log(error);
+    next(errorServ);
   }
 };
