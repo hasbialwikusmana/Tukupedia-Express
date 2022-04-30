2f4afc6c802e55a78e7fad25a5f7d7a580076063
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-01 00:13:28 +0700

    add orders

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const products = require("./routes/products");
 const category = require("./routes/category");
+const orders = require("./routes/orders");
 
 router.use("/products", products);
 router.use("/category", category);
+router.use("/orders", orders);
 
 module.exports = router;
