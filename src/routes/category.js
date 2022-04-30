e56226be79ed8c87297a7d028649143705b1d845
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-30 19:33:11 +0700

    Update router category

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,10 +2,9 @@ const router = require("express").Router();
 const categoryController = require("../controller/category");
 
 router.get("/", categoryController.getCategory);
-router.get("/:id", categoryController.getCategoryById);
+router.get("/:category_id", categoryController.getCategoryById);
 router.post("/", categoryController.postCategory);
-router.put("/:id", categoryController.putCategory);
-router.delete("/:id", categoryController.deleteCategory);
-router.get("/name/:name", categoryController.getCategoryByName);
+router.put("/:category_id", categoryController.putCategory);
+router.delete("/:category_id", categoryController.deleteCategory);
 
 module.exports = router;
