381008b9c6747954e5aa8333a8aeeb7aec54e170
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-30 19:33:37 +0700

    update models category

--- src/models/category.js
+++ src/models/category.js
@@ -5,36 +5,21 @@ const getCategory = ({ limit, offset }) => {
     connection.query(
       "SELECT * FROM category LIMIT $1 OFFSET $2",
       [limit, offset],
-      (err, result) => {
-        if (!err) {
+      (error, result) => {
+        if (!error) {
           resolve(result.rows);
         } else {
-          reject(new Error(err));
+          reject(new Error(error));
         }
       }
     );
   });
 };
-const getCategoryById = (id) => {
+const getCategoryById = (category_id) => {
   return new Promise((resolve, reject) => {
     connection.query(
-      "SELECT * FROM category WHERE id = $1",
-      [id],
-      (err, result) => {
-        if (!err) {
-          resolve(result.rows);
-        } else {
-          reject(new Error(err));
-        }
-      }
-    );
-  });
-};
-const getCategoryByName = (name_category) => {
-  return new Promise((resolve, reject) => {
-    connection.query(
-      "SELECT * FROM category WHERE name_category = $1",
-      [name_category],
+      "SELECT * FROM category WHERE category_id = $1",
+      [category_id],
       (err, result) => {
         if (!err) {
           resolve(result.rows);
@@ -48,11 +33,11 @@ const getCategoryByName = (name_category) => {
 const postCategory = (setData) => {
   return new Promise((resolve, reject) => {
     connection.query(
-      "INSERT INTO category (id, name_category) VALUES ($1, $2)",
-      [setData.id, setData.name_category],
+      "INSERT INTO category (category_name) VALUES ($1)",
+      [setData.category_name],
       (err) => {
         if (!err) {
-          resolve("Success Post Data Category");
+          resolve("Data Succes Added");
         } else {
           reject(new Error(err));
         }
@@ -60,14 +45,15 @@ const postCategory = (setData) => {
     );
   });
 };
-const putCategory = (id, setData) => {
+
+const putCategory = (setData, category_id) => {
   return new Promise((resolve, reject) => {
     connection.query(
-      "UPDATE category SET name_category = $1 WHERE id = $2",
-      [setData.name_category, id],
+      "UPDATE category SET category_name = $1, category_updated_at = $2 WHERE category_id = $3",
+      [setData.category_name, setData.category_updated_at, category_id],
       (err) => {
         if (!err) {
-          resolve("Success Update Data Category");
+          resolve("Data Success Updated");
         } else {
           reject(new Error(err));
         }
@@ -75,26 +61,31 @@ const putCategory = (id, setData) => {
     );
   });
 };
-const deleteCategory = (id) => {
+
+const deleteCategory = (category_id) => {
   return new Promise((resolve, reject) => {
-    connection.query("DELETE FROM category WHERE id = $1", [id], (err) => {
-      if (!err) {
-        resolve("Success Delete Data Category");
-      } else {
-        reject(new Error(err));
+    connection.query(
+      "DELETE FROM category WHERE category_id = $1",
+      [category_id],
+      (err) => {
+        if (!err) {
+          resolve("Data Success Deleted");
+        } else {
+          reject(new Error(err));
+        }
       }
-    });
+    );
   });
 };
 const countCategory = () => {
   return connection.query("SELECT COUNT(*) AS total FROM category");
 };
+
 module.exports = {
   getCategory,
   getCategoryById,
   postCategory,
   putCategory,
   deleteCategory,
-  getCategoryByName,
   countCategory,
 };
