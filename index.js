95a50f2c5d9d95f072340e9b485383246509d237
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-07 22:15:21 +0700

    add dep helmet and updated port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const cors = require("cors");
 const createError = require("http-errors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
+const PORT = process.env.PORT || 5000;
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
@@ -24,6 +26,4 @@ app.use((err, req, res) => {
     message: messError,
   });
 });
-app.listen(process.env.PORT, () =>
-  console.log(`Server running at on Port: ${process.env.PORT}!`)
-);
+app.listen(PORT, () => console.log(`Server running at on Port: ${PORT}!`));
