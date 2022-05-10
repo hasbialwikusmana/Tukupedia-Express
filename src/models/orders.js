84edef56893a2533a14a454c4060f66f2327b156
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-11 00:37:03 +0700

    update models orders

--- src/models/orders.js
+++ src/models/orders.js
@@ -2,13 +2,16 @@ const connection = require("../config/db");
 
 const getOrders = () => {
   return new Promise((resolve, reject) => {
-    connection.query("SELECT * FROM orders", (err, result) => {
-      if (!err) {
-        resolve(result.rows);
-      } else {
-        reject(err);
+    connection.query(
+      "SELECT orders.orders_id, orders_qty,orders_subtotal, products.products_id,products.products_name,products.products_price FROM orders INNER JOIN products ON orders.products_id = products.products_id",
+      (err, result) => {
+        if (!err) {
+          resolve(result.rows);
+        } else {
+          reject(err);
+        }
       }
-    });
+    );
   });
 };
 const getOrderById = (orders_id) => {
@@ -27,7 +30,24 @@ const getOrderById = (orders_id) => {
   });
 };
 
+const postOrders = (setData) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "INSERT INTO orders (products_id,orders_qty,orders_subtotal) VALUES ($1,$2,$3)",
+      [setData.products_id, setData.orders_qty, setData.orders_subtotal],
+      (err, result) => {
+        if (!err) {
+          resolve(result.rows);
+        } else {
+          reject(new Error(err));
+        }
+      }
+    );
+  });
+};
+
 module.exports = {
   getOrders,
   getOrderById,
+  postOrders,
 };
