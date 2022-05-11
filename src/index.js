bd0e6b51f410251a03670cf122000528b9aac624
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-11 20:46:53 +0700

    add router v2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 
-const products = require("./modules/v1/routes/products");
-const category = require("./modules/v1/routes/category");
-const orders = require("./modules/v1/routes/orders");
+const productsv1 = require("./modules/v1/routes/products");
+const categoryv1 = require("./modules/v1/routes/category");
+const ordersv1 = require("./modules/v1/routes/orders");
 
-router.use("/products", products);
-router.use("/category", category);
-router.use("/orders", orders);
+const productsv2 = require("./modules/v2/routes/products");
+const categoryv2 = require("./modules/v2/routes/category");
+const ordersv2 = require("./modules/v2/routes/orders");
+
+router
+  .use("/products", productsv1)
+  .use("/category", categoryv1)
+  .use("/orders", ordersv1);
+
+router
+  .use("/products", productsv2)
+  .use("/category", categoryv2)
+  .use("/orders", ordersv2);
 
 module.exports = router;
