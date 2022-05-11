22c4fc9a062130584b25db4fe8f3df1943885d84
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-11 22:15:46 +0700

    add route users

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 
+// version 1 for beginer backend
 const productsv1 = require("./modules/v1/routes/products");
 const categoryv1 = require("./modules/v1/routes/category");
 const ordersv1 = require("./modules/v1/routes/orders");
 
+// version 2 for intermediate backend
 const productsv2 = require("./modules/v2/routes/products");
 const categoryv2 = require("./modules/v2/routes/category");
 const ordersv2 = require("./modules/v2/routes/orders");
+const usersv2 = require("./modules/v2/routes/users");
 
 router
   .use("/products", productsv1)
@@ -16,6 +19,7 @@ router
 router
   .use("/products", productsv2)
   .use("/category", categoryv2)
-  .use("/orders", ordersv2);
+  .use("/orders", ordersv2)
+  .use("/users", usersv2);
 
 module.exports = router;
