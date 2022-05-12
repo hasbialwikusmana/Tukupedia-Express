cfa69cea77e120a881d740f199e787e20df902db
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-12 17:35:02 +0700

    Update users.js

--- src/modules/v2/routes/users.js
+++ src/modules/v2/routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const usersController = require("../controller/users");
 
-router.post("/register", usersController.register);
+router
+  .post("/register", usersController.register)
+  .post("/login", usersController.login);
 
 module.exports = router;
