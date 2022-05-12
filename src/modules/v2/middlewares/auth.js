13ab301341b45940b8ca43447ff525242a361c0d
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-12 21:33:20 +0700

    Update protect general, isAdmin, env expires

--- src/modules/v2/middlewares/auth.js
+++ src/modules/v2/middlewares/auth.js
@@ -18,29 +18,39 @@ const generateToken = (payload) => {
 const protect = (req, res, next) => {
   try {
     let token;
-    if (req.headers.authorization) {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
       token = req.headers.authorization.split(" ")[1];
 
       let decoded = jwt.verify(token, process.env.SECRET_KEY);
-      console.log(decoded);
+      req.decoded = decoded;
       next();
     } else {
-      next(createError(403, "Server need token"));
+      next(createError(400, "Please login to access this page"));
     }
   } catch (error) {
     console.log(error.name);
-
-    if (error && error.name === "TokenExpiredError") {
-      next(createError(403, "Token invalid"));
-    } else if (error && error.name === "JsonWebTokenError") {
-      next(createError(403, "Token Expired"));
+    if (error.name === "JsonWebTokenError") {
+      next(createError(400, "Token is invalid"));
+    } else if (error.name === "TokenExpiredError") {
+      next(createError(400, "Token is expired"));
     } else {
-      next(createError(403, "Token not active"));
+      next(createError(400, "Token not active"));
     }
   }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.decoded.users_role !== 1) {
+    return next(createError(400, "You are not access to this admin page"));
+  }
+  next();
 };
 
 module.exports = {
   generateToken,
   protect,
+  isAdmin,
 };
