d8320e3b0986acbe32721c558cb7c784d97542bf
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-14 20:40:07 +0700

    Update products.js

--- src/modules/v2/routes/products.js
+++ src/modules/v2/routes/products.js
@@ -1,10 +1,30 @@
 const router = require("express").Router();
 const productsController = require("../controller/products");
 const { protect } = require("../middlewares/auth");
+// const uploads = require("../middlewares/multer");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
+});
+
+const uploads = multer({ storage: storage });
 
 router.get("/", protect, productsController.getProducts);
 router.get("/:products_id", productsController.getProductsById);
-router.post("/", productsController.postProducts);
+router.post(
+  "/",
+  uploads.single("products_images"),
+  productsController.postProducts
+);
 router.put("/:products_id", productsController.putProducts);
 router.delete("/:products_id", productsController.deleteProducts);
 
